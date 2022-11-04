da49461853cf14ea3b7f3c35d2eb9ca11de1a5c6
NgoDinhDai <ngodinhdai77@gmail.com>
2022-11-04 13:51:53 +0700

    add constants

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -3,4 +3,10 @@ export const theme = {
   secondary: "#1200FF",
   grayDark: "#292D32",
   grayLight: "#E7ECF3",
+  tertiary: "#3A1097",
+  accent: "#00D1ED",
+  grayF3: "#F3EDFF",
+  gray6B: "#6b6b6b",
+  gray23: "#232323",
+  gray80: "#808191",
 };
