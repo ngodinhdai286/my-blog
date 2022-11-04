4f5490f3cb91d6acc6d6e5e26c22a9c7918fa6dc
NgoDinhDai <ngodinhdai77@gmail.com>
2022-11-04 13:57:28 +0700

    update code HomePage

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -2,13 +2,21 @@ import { signOut } from "firebase/auth";
 import React from "react";
 import styled from "styled-components";
 import Header from "../components/layout/Header";
+import Layout from "../components/layout/Layout";
+import HomeBanner from "../components/module/home/HomeBanner";
+import HomeFeature from "../components/module/home/HomeFeature";
+import HomeNewest from "../components/module/home/HomeNewest";
 import { auth } from "../firebase/firebase-config";
 const HomePageStyles = styled.div``;
 
 const HomePage = () => {
   return (
     <HomePageStyles>
-      <Header></Header>
+      <Layout>
+        <HomeBanner></HomeBanner>
+        <HomeFeature></HomeFeature>
+        <HomeNewest></HomeNewest>
+      </Layout>
     </HomePageStyles>
   );
 };
