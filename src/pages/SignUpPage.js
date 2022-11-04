8cda31462bfdae5f06e8783eaebec507757c4819
Ngô Đình Đại <ngodinhdai77@gmail.com>
2022-11-05 03:04:45 +0700

    update code with sign up return userId

--- src/pages/SignUpPage.js
+++ src/pages/SignUpPage.js
@@ -10,7 +10,7 @@ import { toast } from "react-toastify";
 import { createUserWithEmailAndPassword, updateProfile } from "firebase/auth";
 import { auth, db } from "../firebase/firebase-config";
 import { NavLink, useNavigate } from "react-router-dom";
-import { addDoc, collection } from "firebase/firestore";
+import { addDoc, collection, doc, setDoc } from "firebase/firestore";
 import AuthenticationPage from "./AuthenticationPage";
 import InputPasswordToggle from "../components/input/InputPasswordToggle";
 import { Field } from "../components/field";
@@ -55,11 +55,16 @@ const SignUpPage = () => {
       displayName: values.fullname,
     });
     const colRef = collection(db, "users");
-    await addDoc(colRef, {
+    await setDoc(doc(db, "users", auth.currentUser.uid), {
       fullname: values.fullname,
       email: values.email,
       password: values.password,
     });
+    // await addDoc(colRef, {
+    //     fullname: values.fullname,
+    //   email: values.email,
+    //   password: values.password,
+    // });
     toast.success("Register successfully!");
     navigate("/");
 
