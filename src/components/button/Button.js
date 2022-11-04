c8b888afcaf878695b0832a74f18f5e5b57bbd67
NgoDinhDai <ngodinhdai77@gmail.com>
2022-11-04 13:48:03 +0700

    update code button

--- src/components/button/Button.js
+++ src/components/button/Button.js
@@ -1,7 +1,8 @@
 import React from "react";
-import styled from "styled-components";
+import styled, { css } from "styled-components";
 import LoadingSpinner from "../loading/LoadingSpinner";
 import PropTypes from "prop-types";
+import { NavLink } from "react-router-dom";
 
 const ButtonStyles = styled.button`
   display: flex;
@@ -15,11 +16,23 @@ const ButtonStyles = styled.button`
   font-size: 18px;
   font-weight: 600;
   height: ${(props) => props.height || "66px"};
-  background-image: linear-gradient(
-    to right bottom,
-    ${(props) => props.theme.primary},
-    ${(props) => props.theme.secondary}
-  );
+
+  ${(props) =>
+    props.kind === "secondary" &&
+    css`
+      background-color: #dff6ff;
+      color: ${(props) => props.theme.secondary};
+    `};
+  ${(props) =>
+    props.kind === "primary" &&
+    css`
+      color: white;
+      background-image: linear-gradient(
+        to right bottom,
+        ${(props) => props.theme.primary},
+        ${(props) => props.theme.secondary}
+      );
+    `};
   &:disabled {
     opacity: 0.5;
     pointer-events: none;
@@ -37,22 +50,33 @@ const Button = ({
   type = "button",
   onClick = () => {},
   children,
+  kind = "primary",
   ...props
 }) => {
-  const { isLoading } = props;
+  const { isLoading, to } = props;
   const child = !!isLoading ? <LoadingSpinner></LoadingSpinner> : children;
+  if (to !== "" && typeof to === "string") {
+    return (
+      <NavLink className="inline-block" to={to}>
+        <ButtonStyles type={type} kind={kind} {...props}>
+          {child}
+        </ButtonStyles>
+      </NavLink>
+    );
+  }
   return (
-    <ButtonStyles type={type} onClick={onClick} {...props}>
+    <ButtonStyles type={type} kind={kind} onClick={onClick} {...props}>
       {child}
     </ButtonStyles>
   );
 };
 
 Button.propTypes = {
-  type: PropTypes.oneOf(["button", "submit"]).isRequired,
+  type: PropTypes.oneOf(["button", "submit"]),
   isLoading: PropTypes.bool,
   onClick: PropTypes.func,
   children: PropTypes.node,
+  kind: PropTypes.oneOf(["primary", "secondary"]),
 };
 
 export default Button;
