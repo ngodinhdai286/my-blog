d0e4584bfe1b367fafcdd7c6802893d283df3a29
Ngô Đình Đại <ngodinhdai77@gmail.com>
2022-11-05 03:10:50 +0700

    fix code to show outstanding posts

--- src/components/module/home/HomeFeature.js
+++ src/components/module/home/HomeFeature.js
@@ -1,18 +1,49 @@
-import React from "react";
+import {
+  collection,
+  limit,
+  onSnapshot,
+  query,
+  where,
+} from "firebase/firestore";
+import React, { useState } from "react";
+import { useEffect } from "react";
 import styled from "styled-components";
+import { db } from "../../../firebase/firebase-config";
 import Heading from "../../layout/Heading";
 import PostFeatureItem from "../post/PostFeatureItem";
 const HomeFeatureStyles = styled.div``;
 
 const HomeFeature = () => {
+  const [posts, setPosts] = useState([]);
+  useEffect(() => {
+    const colRef = collection(db, "posts");
+    const queries = query(
+      colRef,
+      where("status", "==", 1),
+      where("hot", "==", true),
+      limit(3)
+    );
+    onSnapshot(queries, (snapshot) => {
+      const results = [];
+      snapshot.forEach((doc) => {
+        // console.log(doc.data());
+        results.push({
+          id: doc.id,
+          ...doc.data(),
+        });
+      });
+      setPosts(results);
+    });
+  }, []);
+  if (posts.length <= 0) return null;
   return (
     <HomeFeatureStyles className="home-block">
       <div className="container">
         <Heading>Bài viết nổi bật</Heading>
         <div className="grid-layout">
-          <PostFeatureItem></PostFeatureItem>
-          <PostFeatureItem></PostFeatureItem>
-          <PostFeatureItem></PostFeatureItem>
+          {posts.map((post) => (
+            <PostFeatureItem key={post.id} data={post}></PostFeatureItem>
+          ))}
         </div>
       </div>
     </HomeFeatureStyles>
