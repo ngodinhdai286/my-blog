01e5d08fd6ab9ce84bacbceff3a112458d3c9edb
Ngô Đình Đại <ngodinhdai77@gmail.com>
2022-11-09 02:55:14 +0700

    add more routes

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
-import { Route, Routes } from "react-router-dom";
-import DashboardLayout from "./components/module/dashboard/DashboardLayout";
-import PostAddNew from "./components/module/post/PostAddNew";
-import PostManage from "./components/module/post/PostManage";
-import { AuthProvider } from "./contexts/auth-context";
-import DashboardPage from "./pages/DashboardPage";
-import HomePage from "./pages/HomePage";
-import NotFoundPage from "./pages/NotFoundPage";
-import PostDetailsPage from "./pages/PostDetailsPage";
-import SignInPage from "./pages/SignInPage";
 import SignUpPage from "./pages/SignUpPage";
+import SignInPage from "./pages/SignInPage";
+import PostManage from "./components/module/post/PostManage";
+import PostDetailsPage from "./pages/PostDetailsPage";
+import PostAddNew from "./components/module/post/PostAddNew";
+import NotFoundPage from "./pages/NotFoundPage";
+import HomePage from "./pages/HomePage";
+import DashboardPage from "./pages/DashboardPage";
+import DashboardLayout from "./components/module/dashboard/DashboardLayout";
+import { Route, Routes } from "react-router-dom";
+// import { AuthProvider } from "./contexts/auth-context";
+import CategoryManage from "./components/module/category/CategoryManage";
+import CategoryAddNew from "./components/module/category/CategoryAddNew";
+import UserManage from "components/module/user/UserManage";
+import { AuthProvider } from "contexts/auth-context";
+import CategoryUpdate from "components/module/category/CategoryUpdate";
+import UserAddNew from "components/module/user/UserAddNew";
+import UserProfile from "components/module/user/UserProfile";
+import UserUpdate from "components/module/user/UserUpdate";
 
 function App() {
   return (
@@ -35,6 +43,34 @@ function App() {
             <Route
               path="/manage/add-post"
               element={<PostAddNew></PostAddNew>}
+            ></Route>
+            <Route
+              path="/manage/category"
+              element={<CategoryManage></CategoryManage>}
+            ></Route>
+            <Route
+              path="/manage/add-category"
+              element={<CategoryAddNew></CategoryAddNew>}
+            ></Route>
+            <Route
+              path="/manage/update-category"
+              element={<CategoryUpdate></CategoryUpdate>}
+            ></Route>
+            <Route
+              path="/manage/user"
+              element={<UserManage></UserManage>}
+            ></Route>
+            <Route
+              path="/manage/add-user"
+              element={<UserAddNew></UserAddNew>}
+            ></Route>
+            <Route
+              path="/manage/update-user"
+              element={<UserUpdate></UserUpdate>}
+            ></Route>
+            <Route
+              path="/profile"
+              element={<UserProfile></UserProfile>}
             ></Route>
           </Route>
         </Routes>
